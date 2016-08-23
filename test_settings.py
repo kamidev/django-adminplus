98193aa3a1d6de8738b3d1360df99b4d63a07841
kamidev <jonas.beckman@gmail.com>
2016-08-23 16:57:04 +0200

    Add contextprocessor for TEMPLATES tag

--- test_settings.py
+++ test_settings.py
@@ -17,23 +17,29 @@ DATABASES = {
 }
 
 TEMPLATES = [
-            {
-                        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-                                'APP_DIRS': True,
-                                    },
-            ]
-'context_processors': [
-    # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
-    # list if you haven't customized them:
-    'django.contrib.auth.context_processors.auth',
-    'django.template.context_processors.debug',
-    'django.template.context_processors.i18n',
-    'django.template.context_processors.media',
-    'django.template.context_processors.static',
-    'django.template.context_processors.tz',
-    'django.contrib.messages.context_processors.messages',
-    'django.contrib.auth.context_processors.request', # <-- Here we add the request
-],
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            join(BASE_DIR, 'templates'),
+            # insert more TEMPLATE_DIRS here
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
+                # list if you haven't customized them:
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+                'django.contrib.auth.context_processors.request',
+            ],
+        },
+    },
+]
 	
 ROOT_URLCONF = 'test_urlconf'
 MIDDLEWARE_CLASSES = ()
