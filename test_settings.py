51d272372cee68b72d4eb17ac78fbfcf64532f82
kamidev <jonas.beckman@gmail.com>
2016-08-23 16:48:13 +0200

    Add contextprocessor for templates

--- test_settings.py
+++ test_settings.py
@@ -22,6 +22,18 @@ TEMPLATES = [
                                 'APP_DIRS': True,
                                     },
             ]
-
+'context_processors': [
+    # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
+    # list if you haven't customized them:
+    'django.contrib.auth.context_processors.auth',
+    'django.template.context_processors.debug',
+    'django.template.context_processors.i18n',
+    'django.template.context_processors.media',
+    'django.template.context_processors.static',
+    'django.template.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+    'django.contrib.auth.context_processors.request', # <-- Here we add the request
+],
+	
 ROOT_URLCONF = 'test_urlconf'
 MIDDLEWARE_CLASSES = ()
